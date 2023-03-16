303bb1387c7f58e6859359c081355f7f593bbeee
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2023-03-16 14:19:28 -0700

    add `utime`

--- src/main.c
+++ src/main.c
@@ -1,9 +1,21 @@
 #include "config.h"
 
+#include "main.h"
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+static inline int64_t
+utime(void)
+{
+  struct timespec t;
+  clock_gettime( CLOCK_REALTIME, &t );
+  return (int64_t)( t.tv_sec ) * 1000000 + (int64_t)( t.tv_nsec / 1000 );
+}
 
 int main(int argc, char **argv) {
-    printf("Aloha! from %s in %s v%s\n", argv[0], PROJECT_NAME, PROJECT_VERSION);
-    return EXIT_SUCCESS;
+  int64_t t = utime();
+  printf("Aloha! at %" PRId64 " from %s in %s v%s\n", t, argv[0], PROJECT_NAME, PROJECT_VERSION);
+  return EXIT_SUCCESS;
 }
---
+++ src/main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#define PERIOD 1e-2
+
+#include <inttypes.h>
+
+#include <lcm/lcm.h>
+
+
+static inline int64_t utime(void);
+
+static void random_walker(void* data);
+
+#endif // _MAIN_H_
