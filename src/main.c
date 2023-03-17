76aa71c01f517f2dbde0f2e19f98693122aba6a5
M Jordan Stanway <m.j.stanway@alum.mit.edu>
2023-03-16 17:51:01 -0700

    actually use LCM

--- src/main.c
+++ src/main.c
@@ -2,11 +2,13 @@
 
 #include "main.h"
 
+#include <math.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+
 
 static inline int64_t
 utime(void)
@@ -16,27 +18,99 @@ utime(void)
   return (int64_t)( t.tv_sec ) * 1000000 + (int64_t)( t.tv_nsec / 1000 );
 }
 
+static void lprint(const lcm_recv_buf_t* rbuf,
+          const char* channel,
+          const minmt_example_t* rx,
+          void* user){
+  fprintf(stderr, "lprint\n");
+  lcm_t* lcm = (lcm_t*)(user);
+
+  minmt_example_t_publish(lcm, FOLLOWER_CHANNEL, rx);
+}
+
+static void
+follow(const lcm_recv_buf_t* rbuf,
+          const char* channel,
+          const minmt_example_t* rx,
+          void* user){
+  follower_t* f = (follower_t*)(user);
+  lcm_t* lcm = (lcm_t*)(f->lcm);
+  minmt_example_t* self = (minmt_example_t*)(f->self);
+
+  double delta=0.0, step=1.0;
+
+  for (int dof = 0; dof < 2; dof++) {
+      delta = rx->position[dof] - self->position[dof];
+      self->position[dof] += copysign(delta, step);
+  }
+
+  // TODO if self->position is within capture radius, disable self
+
+  // TODO consider publishing on different channels
+  minmt_example_t_publish(lcm, FOLLOWER_CHANNEL, self);
+}
+
 static void*
-follow (void* data){
+follower_loop (void* data){
+  int handled = 1;
   int n = *(int *)data;
   int64_t t = 0;
-  for (int i=0; i<REPETITIONS; i++) {
-      t = utime();
-      printf("Aloha! at %" PRId64 " from thread %d in %s v%s\n", t, n, PROJECT_NAME, PROJECT_VERSION);
-      sleep(PERIOD);
+
+  follower_t follower = { 0 };
+
+  lcm_t* lcm = lcm_create(provider);
+  if (!lcm) {
+    pthread_exit(NULL); // TODO EXIT_FAILURE?
   }
+
+  minmt_example_t self = { 0 };
+
+  follower.lcm = lcm;
+  follower.self = &self;
+
+//  minmt_example_t_subscription_t* _sub = minmt_example_t_subscribe(lcm, ATTRACTOR_CHANNEL, &lprint, (void*)(lcm));
+
+  minmt_example_t_subscription_t* sub = minmt_example_t_subscribe(lcm, ATTRACTOR_CHANNEL, &follow, (void*)(&follower));
+  minmt_example_t_subscription_set_queue_capacity(sub, LCM_QUEUE_CAPACITY);
+
+  while(handled > 0) { // TODO consider using minmt_example_t->enabled to signal time to close the thread
+    handled = lcm_handle_timeout(lcm, LCM_HANDLE_TIMEOUT);
+  }
+
+  lcm_destroy(lcm);
   return NULL; // TODO Is this really necessary?
 }
 
 int main(int argc, char **argv) {
+
+  provider = getenv("LCM_DEFAULT_URL");
+  if(provider) {
+    printf("using LCM provider %s\n", provider);
+  } else {
+    printf("using LCM provider NULL\n");
+  }
+
+  lcm_t* lcm = lcm_create(provider);
+  if (!lcm) {
+    exit(EXIT_FAILURE);
+  }
+
+  minmt_example_t attractor = { 0 };
+  attractor.enabled = 1;
+
   int _rc = 0;
   pthread_t follower[N_FOLLOWERS];
 
   for (int i=0; i<N_FOLLOWERS; i++) {
-    _rc = pthread_create(&follower[i], NULL, follow, (void *)&i);
+    _rc = pthread_create(&follower[i], NULL, follower_loop, (void *)&i);
     if (_rc != 0) {
         perror("pthread_create");
     }
+  }
+
+  for (int i=0; i<REPETITIONS; i++) {
+    minmt_example_t_publish(lcm, ATTRACTOR_CHANNEL, &attractor);
+    usleep(PERIOD);
   }
 
   for (int i=0; i<N_FOLLOWERS; i++) {
--- src/main.h
+++ src/main.h
@@ -1,17 +1,35 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#define PERIOD 1e-2
-#define REPETITIONS 3
-#define N_FOLLOWERS 7 // same as # colors in ROYGBIV
+
+#define LCM_QUEUE_CAPACITY 3
+#define LCM_HANDLE_TIMEOUT 1000 // milliseconds
+
+#define PERIOD 10000 // microseconds (for usleep in main thread)
+#define REPETITIONS 1000
+
+#define N_FOLLOWERS 7 // 7 same as # colors in ROYGBIV
+
+#define ATTRACTOR_CHANNEL "A"
+#define FOLLOWER_CHANNEL "F"
 
 #include <inttypes.h>
 
 #include <lcm/lcm.h>
+#include <minmt_example_t.h>
 
+static char* provider = NULL; // This is global so that all threads use the same LCM provider.
 
 static inline int64_t utime(void);
 
-static void random_walker(void* data);
+typedef struct _follower_t {
+  lcm_t* lcm;
+  minmt_example_t* self;
+} follower_t;
+//^ For a truly minimal example, we could have avoided this by choosing a
+//  problem that did not need to track any internal state.
+
+static void follow(const lcm_recv_buf_t* rbuf, const char* channel, const minmt_example_t* rx, void* user);
+static void* follower_loop(void* data);
 
 #endif // _MAIN_H_
